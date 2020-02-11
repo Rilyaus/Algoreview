9fb8ea2cfab88983c5f5206aeb907944303a0ba6
Rilyaus <rilyaus.gd@gmail.com>
2020-02-11 18:23:41 +0900

    Al01Seq: update code

--- Al01Seq.java
+++ Al01Seq.java
@@ -1,12 +1,31 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Al01Seq {
-    public static void main(String[] args) {
+    static public void main(String[] args) {
         Scanner sca = new Scanner(System.in);
 
-        String str = sca.nextLine();
-        System.out.println(str);
-
+        int start = sca.nextInt();
+        int end = sca.nextInt();
+        
         sca.close();
+
+        int max = 0;
+
+        for( int i=start ; i<=end ; i++ ) {
+            int temp = seq(i, 0);
+            max = temp > max ? temp : max;
+        }
+
+        System.out.println(start + " " + end + " " + max);
+    }
+
+    static public int seq(int num, int count) {
+        if ( num == 1 ) {
+            return count+1;
+        } else if( (num % 2) == 0 ) {
+            return seq((num/2), count+1);
+        } else {
+            return seq((num * 3 + 1), count+1);
+        }
     }
 }
