ee8e063335ab7d78ef738f4826e67589597c2e0b
Rilyaus <rilyaus.gd@gmail.com>
2020-02-11 12:52:55 +0900

    shortcut Test

---
+++ Al01Seq.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Al01Seq {
+    public static void main(String[] args) {
+        Scanner sca = new Scanner(System.in);
+
+        String str = sca.nextLine();
+        System.out.println(str);
+
+        sca.close();
+    }
+}
